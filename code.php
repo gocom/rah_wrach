2ee7e61c4ff7398fbd656cc248e4af4931048bd7
Jukka Svahn <web@rahinaa.biz>
2012-07-27 04:32:53 +0300

    Adding v0.2 from SVN.

--- code.php
+++ code.php
@@ -1,42 +1,110 @@
 <?php	##################
 	#
 	#	rah_write_each_section-plugin for Textpattern
-	#	version 0.1
+	#	version 0.2
 	#	by Jukka Svahn
 	#	http://rahforum.biz
 	#
 	###################
 
-	if (@txpinterface == 'admin') register_callback('rah_write_each_section','article');
+	if(@txpinterface == 'admin') {
+		register_callback('rah_write_each_section','article');
+		register_callback('rah_write_each_section_head','admin_side','head_end');
+	}
+
+/**
+	Add styles and JavaScript to the <head>
+*/
+
+	function rah_write_each_section_head() {
+		
+		global $event;
+		
+		if($event != 'article')
+			return;
+
+		extract(
+			gpsa(
+				array(
+					'ID',
+					's',
+					'Section',
+					'view'
+				)
+			)
+		);
+
+		echo <<<EOF
+			<style type="text/css">
+				#rah_write_each_section_container {
+					width: 350px;
+					margin: 15px auto;
+				}
+			</style>
+EOF;
+
+		if(!$ID && !$Section && $s && $s != 'all_site_sections_opened_for_posting') {
+			$value = str_replace('"','\"',$s);
+			echo <<<EOF
+				<script language="javascript" type="text/javascript">
+					$(document).ready(function() {
+						$("#section option[value={$value}]").attr("selected","selected");
+						$("#write-sort p:last").css("display","none");
+					});
+				</script>
+EOF;
+		}
+
+		if(!$s && !$Section && !$ID && !$view) {
+			echo <<<EOF
+				<script type="text/javascript">
+					$(document).ready(function() {
+						$("form[name=article], form#article_form").remove();
+					});
+				</script>
+EOF;
+		}
+	}
+
+/**
+	Adds the extra step to Write panel
+*/
 
 	function rah_write_each_section() {
-		$section = gps('s');
-		if(!gps('ID') && !ps('ID') && gps('s') && !ps('Section') && $section != 'all_site_sections_opened_for_posting') {
+		
+		extract(
+			gpsa(
+				array(
+					'ID',
+					's',
+					'Section',
+					'view'
+				)
+			)
+		);
+
+		if(!$s && !$Section && !$ID && !$view) {
 			echo 
-				'<script language="javascript" type="text/javascript">'.n.
-				'	$(document).ready(function() {'.n.
-				'		$("#section option[value='.str_replace('"','\"',$section).']").attr("selected","selected");'.n.
-				'		$("#write-sort p:last").css("display","none");'.n.
-				'	});'.n.
-				'</script>';
-		}
-		$view = gps('view');
-		if(!gps('s') && !ps('Section') && !gps('ID') && !$view) {
-			echo 
-				'<h1 style="width:600px;margin:15px auto;">'.gTxt('tab_write').' &#8250; '.gTxt('tab_sections').' &#8250; '.gTxt('select').'</h1>'.n.
-				'<ul style="width:600px;margin:0 auto;">'.n;
-			$rs = safe_rows_start('title, name', 'txp_section',"name != 'default' and in_rss = '1' order by title ASC");
-			while ($a = nextRow($rs)) {
+				'<div id="rah_write_each_section_container">'.n.
+				'	<h1>'.gTxt('tab_write').' &#8250; '.gTxt('tab_sections').' &#8250; '.gTxt('select').'</h1>'.n.
+				'	<ul>'.n;
+			
+			$rs = 
+				safe_rows(
+					'title,name',
+					'txp_section',
+					"name != 'default' and in_rss = '1' order by title ASC"
+				);
+
+			foreach($rs as $a) {
 				extract($a);
-				echo '	<li><a href="?event=article&amp;s='.$name.'">'.$title.'</a></li>'.n;
+				echo 
+					'		<li><a href="?event=article&amp;s='.htmlspecialchars($name).'">'.htmlspecialchars($title).'</a></li>'.n;
 			}
+
 			echo 
-				'	<li><strong><a href="?event=article&amp;s=all_site_sections_opened_for_posting">'.gTxt('advanced_options').'</a></strong></li>'.n.
-				'</ul>'.n.
-				'<script language="javascript" type="text/javascript">'.n.
-				'	$(document).ready(function() {'.n.
-				'		$("form[name=article]").remove();'.n.
-				'	});'.n.
-				'</script>';
+				'		<li><strong><a href="?event=article&amp;s=all_site_sections_opened_for_posting">'.gTxt('advanced_options').'</a></strong></li>'.n.
+				'	</ul>'.n.
+				'</div>'.n;
 		}
 	}
